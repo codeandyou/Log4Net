79909fb8e9b2e2f8499782c9693f393ab2a12cbf
virender verma <virenderverma@live.com>
2015-02-26 07:33:01 +0530

    Added "How to Use log4net in ASP.NET MVC" project
    Added "How to Use log4net in ASP.NET MVC" project

---
+++ Log4NetMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Log4NetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static log4net.ILog Log { get; set; }
+
+        ILog log = log4net.LogManager.GetLogger(typeof(HomeController));      //type of class
+
+        //log4net demo
+        public ActionResult Index()
+        {
+            log.Debug("Debug message");
+            log.Warn("Warn message");
+            log.Error("Error message");
+            log.Fatal("Fatal message");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Log4NetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Log4NetMVC.Startup))]
+namespace Log4NetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
